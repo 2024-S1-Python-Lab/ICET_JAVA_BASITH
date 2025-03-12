54dd415897a6c8d35bc53f07fd4c358221b02d36
basithks <abdulbasith0485@gmail.com>
2025-03-12 14:08:43 +0530

    Add files via upload

---
+++ IfEg.java
@@ -0,0 +1,11 @@
+public class IfEg 
+			{
+   				 public static void main(String[] args) 
+   				 {
+       				 int num = 10;
+       				 if (num > 0)     // Condition: true
+        			 {  
+           				 System.out.println("The number is positive.");
+       			 	 }
+   		         }
+			}
